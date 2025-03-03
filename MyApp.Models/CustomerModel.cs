907451e1c78e282f94b752dcaf73ffc1263b7b11
Anmol <Anmol@RYZEN73800X>
2025-03-03 19:30:11 +0530

    Add project files.

---
+++ MyApp.Models/CustomerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace MyApp.Models
+{
+    public class CustomerModel
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email_Id { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
---
+++ MyApp.Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace MyApp.Models
+{
+    public class LoginModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
